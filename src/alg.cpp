fcc3b7bef34b40e3da105c9e2e518218b0ceaac9
denis-vyzulin <denis-vyzulin@mail.ru>
2026-06-09 12:50:02 +0300

    edit alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,45 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+  uint64_t res = num;
+  while (num != 1) {
+    if (num % 2) {
+      num = 3 * num + 1;
+      res = max(res, num);
+    }
+    while (num % 2 == 0) num /= 2;
+  }
+  return res;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  if (num <= 0) return 0;
+  unsigned int res = 1;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    }
+    else {
+      num = 3 * num + 1;
+    }
+    res++;
+  }
+  return res;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+  if (rbound % 2 == 0) rbound--;
+  if (lbound % 2 == 0) lbound++;
+  uint64_t num = 1;
+  while (rbound >= lbound) {
+    unsigned int len = collatzLen(rbound);
+    if (*maxlen < len) {
+      *maxlen = len;
+      num = collatzMaxValue(rbound);
+    }
+    rbound -= 2;
+  }
+  return num;
 }
